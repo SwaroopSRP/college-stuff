58f2fb50029ea7ff2839e433ccbf45cef9876f52
SRP <140191636+SwaroopSRP@users.noreply.github.com>
2024-10-08 09:30:41 +0530

    Create bst.c

---
+++ DSA/bst.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+Node* minValueNode(Node* node) {
+    Node* current = node;
+    while (current && current->left != NULL) {
+        current = current->left;
+    }
+    return current;
+}
+
+Node* deleteNode(Node* root, int data) {
+    if (root == NULL) {
+        return root;
+    }
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    } else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        Node* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    printf("Inorder traversal of the BST: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Delete 20\n");
+    root = deleteNode(root, 20);
+    printf("Inorder traversal after deleting 20: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Delete 30\n");
+    root = deleteNode(root, 30);
+    printf("Inorder traversal after deleting 30: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Delete 50\n");
+    root = deleteNode(root, 50);
+    printf("Inorder traversal after deleting 50: ");
+    inorder(root);
+    printf("\n");
+
+    return 0;
+}
